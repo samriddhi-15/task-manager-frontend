beaa8ffb4ea93344f29c8619f4bf4930debd997d
Samriddhi Kaushal <samriddhikaushal15@gmail.com>
2026-05-01 23:12:43 +0530

    fixed build errors

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,6 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  test: {
-    environment: 'jsdom',
-    setupFiles: './src/setupTests.ts',
-    globals: true,
-  },
-});
+  plugins: [react()]
+})
